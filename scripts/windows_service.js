bab75eb048a05a78c6975f776f37b5d0722d5759
Andrej Sykora <andrej@bohemian.studio>
2023-09-27 18:19:48 +0200

    feat: windows service setup script

---
+++ scripts/windows_service.js
@@ -0,0 +1,20 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'KA',
+  description: 'KA',
+  script: 'C:\\dev\\ka\\src\\server.js',
+  nodeOptions: [
+  ]
+  //, workingDirectory: '...'
+  //, allowServiceLogon: true
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
